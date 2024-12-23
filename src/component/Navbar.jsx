5f9b3c54a7f0d98acf98d6fc48470623345b6c0c
ravigupta <rg187888@gmail.com>
2024-12-23 20:04:46 +0530

    add menu and it's fully completed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  console.log(menu);
+
   return (
     <div className="h-[18vh] w-full relative  text-white overflow-hidden">
       <div className="h-full w-full flex justify-between items-center px-10 ">
@@ -36,14 +40,46 @@ const Navbar = () => {
           ))}
         </div>
 
-        {/* Menu */}
-        <div className="flex gap-4 items-center justify-center">
+        {/* MenuButton */}
+        <div
+          onClick={() => setMenu((prev) => !prev)}
+          className="flex gap-4 items-center justify-center"
+        >
           <h1 className="p-4 hidden lg:block uppercase text-md font-medium">
             Shop
           </h1>
           <span className="font-medium">
             <IoIosMenu size={25} />
           </span>
+        </div>
+
+        {/* Menu */}
+        <div
+          className={`fixed top-0 right-0 h-full bg-black text-white w-[25%] transform ${menu ? "visible" : "hidden"} 
+          transition-transform duration-150 ease text-right px-10`}
+        >
+          <div className="mt-10">
+            <button
+              className="text-white text-lg font-bold mb-4"
+              onClick={() => setMenu(false)}
+            >
+              <span className="font-medium">
+                <IoClose size={25} />
+              </span>
+            </button>
+            <ul className="flex flex-col gap-4">
+              {["Mission", "Launches", "Careers", "Updates", "Shop"].map(
+                (liItems, liIndex) => (
+                  <li
+                    className="uppercase border-b border-white text-sm font-medium pb-2"
+                    key={liIndex}
+                  >
+                    {liItems}
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
